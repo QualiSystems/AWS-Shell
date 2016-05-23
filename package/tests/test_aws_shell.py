29424e866f90166c9c2d74a3b950d946902e7e14
GilGald <gil.2006@gmail.com>
2016-05-23 11:16:51 +0300

    test deploy

--- package/tests/test_aws_shell.py
+++ package/tests/test_aws_shell.py
@@ -33,17 +33,13 @@ class TestAWSShell(TestCase):
         self.aws_shell_api.deploy_ami_operation.deploy = Mock(return_value=(result, name))
 
         aws_cloud_provider = AWSEc2CloudProviderResourceModel()
-        res = self.aws_shell_api.deploy_ami(deploymock, aws_cloud_provider)
 
-        ami_res_name = jsonpickle.decode(res)['vm_name']
-        instance_id = jsonpickle.decode(res)['vm_uuid']
-
-        self.assertEqual(ami_res_name, name)
-        self.assertEqual(instance_id, result.instance_id)
-        self.assertEqual(instance_id, result.instance_id)
-        self.assertEqual(jsonpickle.decode(res)['auto_power_on'], deploymock.auto_power_on)
-        self.assertEqual(jsonpickle.decode(res)['auto_power_off'], deploymock.auto_power_off)
-        self.assertEqual(jsonpickle.decode(res)['wait_for_ip'], deploymock.wait_for_ip)
-        self.assertEqual(jsonpickle.decode(res)['auto_delete'], deploymock.auto_delete)
-        self.assertEqual(jsonpickle.decode(res)['autoload'], deploymock.autoload)
-        self.assertEqual(jsonpickle.decode(res)['cloud_provider_resource_name'],deploymock.aws_ec2)
+        result = self.aws_shell_api.deploy_ami(deploymock, aws_cloud_provider)
+        self.assertEqual(jsonpickle.decode(result)['vm_name'], name)
+        self.assertEqual(jsonpickle.decode(result)['vm_uuid'], result.instance_id)
+        self.assertEqual(jsonpickle.decode(result)['auto_power_on'], deploymock.auto_power_on)
+        self.assertEqual(jsonpickle.decode(result)['auto_power_off'], deploymock.auto_power_off)
+        self.assertEqual(jsonpickle.decode(result)['wait_for_ip'], deploymock.wait_for_ip)
+        self.assertEqual(jsonpickle.decode(result)['auto_delete'], deploymock.auto_delete)
+        self.assertEqual(jsonpickle.decode(result)['autoload'], deploymock.autoload)
+        self.assertEqual(jsonpickle.decode(result)['cloud_provider_resource_name'],deploymock.aws_ec2)
