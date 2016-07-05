df3df25698c1afe535b1cd56f8aaceedcd157814
Raz Abadi <razaba@gmail.com>
2016-07-05 15:01:13 +0300

    fixing bugs

--- package/cloudshell/cp/aws/domain/services/ec2/security_group.py
+++ package/cloudshell/cp/aws/domain/services/ec2/security_group.py
@@ -14,8 +14,9 @@ class SecurityGroupService(object):
             raise
 
     def delete_all_security_groups_of_instance(self, instance):
-        for security_group in instance.security_groups:
-            self.delete_security_group(security_group)
+        if instance.security_groups:
+            for security_group in instance.security_groups:
+                self.delete_security_group(security_group)
 
     def create_security_group(self, ec2_session, vpc_id, security_group_name):
         """
