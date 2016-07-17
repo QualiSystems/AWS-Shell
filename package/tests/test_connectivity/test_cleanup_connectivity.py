ab8cde857c56a3775a14e225bba7f53bc5d884b5
Alex A <aazarh@gmail.com>
2016-07-17 12:55:52 +0300

    fixed failing tests

--- package/tests/test_connectivity/test_cleanup_connectivity.py
+++ package/tests/test_connectivity/test_cleanup_connectivity.py
@@ -12,21 +12,23 @@ class TestCleanupConnectivity(TestCase):
         self.key_pair_serv = Mock()
         self.s3_session = Mock()
         self.ec2_session = Mock()
-        self.bucket_name = Mock()
+        self.aws_ec2_data_model = Mock()
         self.reservation_id = Mock()
-        self.cleanup_operation = CleanupConnectivityOperation(self.vpc_serv, self.key_pair_serv)
+        self.route_table_service = Mock()
+        self.cleanup_operation = CleanupConnectivityOperation(self.vpc_serv, self.key_pair_serv,
+                                                              self.route_table_service)
 
     def test_cleanup(self):
         vpc = self.vpc_serv.find_vpc_for_reservation()
 
         self.cleanup_operation.cleanup(ec2_session=self.ec2_session,
                                        s3_session=self.s3_session,
-                                       bucket_name=self.bucket_name,
+                                       aws_ec2_data_model=self.aws_ec2_data_model,
                                        reservation_id=self.reservation_id)
 
         self.assertTrue(self.vpc_serv.find_vpc_for_reservation.called_with(self.ec2_session, self.reservation_id))
         self.assertTrue(self.key_pair_serv.remove_key_pair_for_reservation.called_with(self.s3_session,
-                                                                                       self.bucket_name,
+                                                                                       self.aws_ec2_data_model,
                                                                                        self.reservation_id))
         self.assertTrue(self.vpc_serv.delete_all_instances.called_with(vpc))
         self.assertTrue(self.vpc_serv.remove_all_security_groups.called_with(vpc))
@@ -37,10 +39,11 @@ class TestCleanupConnectivity(TestCase):
     def test_cleanup_no_vpc(self):
         vpc_serv = Mock()
         vpc_serv.find_vpc_for_reservation = Mock(return_value=None)
-        result = CleanupConnectivityOperation(vpc_serv, self.key_pair_serv).cleanup(
+        result = CleanupConnectivityOperation(vpc_serv, self.key_pair_serv, self.route_table_service)\
+            .cleanup(
                           ec2_session=self.ec2_session,
                           s3_session=self.s3_session,
-                          bucket_name=self.bucket_name,
+                          aws_ec2_data_model=self.aws_ec2_data_model,
                           reservation_id=self.reservation_id)
 
         self.assertFalse(result['success'])
