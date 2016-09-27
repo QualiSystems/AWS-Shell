6b20149a7ddab289b0c0803e8680c54ee599e6b3
Anthony Piddubny <anton.p@qualisystems.com>
2016-09-27 13:11:46 +0300

    Task 110 unclear error message in case of Power on perior to VM termination via the AWS

--- package/cloudshell/cp/aws/domain/services/ec2/instance.py
+++ package/cloudshell/cp/aws/domain/services/ec2/instance.py
@@ -107,7 +107,11 @@ class InstanceService(object):
 
     @staticmethod
     def get_instance_by_id(ec2_session, id):
-        return ec2_session.Instance(id=id)
+        instance = ec2_session.Instance(id=id)
+        if not hasattr(instance, "state") or instance.state['Name'].lower() == 'terminated':
+            raise Exception("Can't perform action. EC2 instance was terminated/removed")
+
+        return instance
 
     @staticmethod
     def allocate_elastic_address(ec2_client):
