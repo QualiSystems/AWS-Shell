7edf25279fc4391f050f1f4f2dcfc16e2891c78b
Alex Azarh <Alex.az@qualisystems.com>
2017-11-29 12:03:38 +0200

    added retry on elastic ip actions

--- package/cloudshell/cp/aws/common/retry_helper.py
+++ package/cloudshell/cp/aws/common/retry_helper.py
@@ -1,6 +1,12 @@
+from botocore.exceptions import ClientError
 from retrying import retry
 
 
 @retry(stop_max_attempt_number=10, wait_fixed=2000)
 def do_with_retry(action):
     action()
+
+
+def retry_if_client_error(exception):
+    """Return True if we should retry (in this case when it's an IOError), False otherwise"""
+    return isinstance(exception, ClientError)
