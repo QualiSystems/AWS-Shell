18ae55946a4584ce633d51ae4e0895c692af3e30
Nahum Timerman <ntimerman@gmail.com>
2018-01-24 17:45:59 +0200

    add minor test

---
+++ package/tests/test_common/test_get_vm_details.py
@@ -0,0 +1,21 @@
+from unittest import TestCase
+from cloudshell.cp.aws.aws_shell import AWSShell
+from mock import Mock, MagicMock, call
+from jsonpickle import encode
+
+
+class TestGetVmDetails(TestCase):
+    def test_get_vm_details(self):
+        shell = AWSShell()
+
+        requests_json = encode({'items': [{'DeployedAppJson': {'Vmdetails': {'UID': '514'}}}]})
+
+        #
+        # requests_json = """{ 'Items': [{ 'DeployedAppJson': {
+        #                             'Vmdetails': { 'Uid': '514' }
+        #     }]
+        #      }
+        # """
+
+
+        shell.get_vm_details(Mock(), Mock(), requests_json)
