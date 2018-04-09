25ebdd7fe887b72df993fb6a5f448a218dddd2fb
alexazarh <alex.az@quali.com>
2018-04-09 12:31:53 +0300

    Increasing retry max attempts

--- package/cloudshell/cp/aws/common/retry_helper.py
+++ package/cloudshell/cp/aws/common/retry_helper.py
@@ -2,7 +2,7 @@ from botocore.exceptions import ClientError
 from retrying import retry
 
 
-@retry(stop_max_attempt_number=10, wait_fixed=2000)
+@retry(stop_max_attempt_number=30, wait_fixed=1000)
 def do_with_retry(action):
     action()
 
