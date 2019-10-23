165b56d7404bf7e3edc1a82ea43866bcb993cd44
Nahum Timerman <ntimerman@gmail.com>
2019-10-23 12:31:13 +0300

    support for cancellation

--- package/tests/test_connectivity/test_create_traffic_mirroring.py
+++ package/tests/test_connectivity/test_create_traffic_mirroring.py
@@ -26,7 +26,7 @@ class TestCreateTrafficMirroring(TestCase):
         except:
             raise Exception('failed to connect to Quali Server')
         # self.res = self.cloudshell.CreateImmediateReservation('test', 'admin', 120).Reservation
-        self.res = self.cloudshell.GetReservationDetails('8d7fa421-9bc3-4719-9acf-d182eabe7cac').ReservationDescription
+        self.res = self.cloudshell.GetReservationDetails('f31aa724-d4e3-49bf-ac8c-405622166e1d').ReservationDescription
         self.fulfillments = []
 
     def tearDown(self):
@@ -34,59 +34,62 @@ class TestCreateTrafficMirroring(TestCase):
         # TrafficMirrorCleaner.rollback(self.ec2_client, self.fulfillments)
         pass
 
-    # def test_create_traffic_mirroring(self):
-    #     tag_service = TagService(Mock())
-    #     session_number_service = SessionNumberService()
-    #     traffic_mirror_service = TrafficMirrorService()
-    #     cancellation_service = CommandCancellationService()
-    #     op = CreateTrafficMirrorOperation(tag_service,
-    #                                       session_number_service,
-    #                                       traffic_mirror_service,
-    #                                       cancellation_service)
-    #
-    #     reservation_context = Mock()
-    #     reservation_context.reservation_id = self.res.Id
-    #     reservation = ReservationModel(reservation_context)
-    #     reservation.blueprint = 'lalala'
-    #     reservation.owner = 'admin'
-    #     reservation.domain = 'global'
-    #
-    #     request = '''
-    #     {
-    #         "driverRequest": {
-    #                             "actions": [
-    #                                             {
-    #                                                 "actionId": "cbe6d3cb-daf3-4ec6-8a26-d2be9d500175",
-    #                                                 "actionTarget": null,
-    #                                                 "type": "CreateTrafficMirroring",
-    #                                                 "actionParams": {"type": "CreateTrafficMirroringParams",
-    #                                                                  "sourceNicId": "eni-0edaa37a4ccefc330",
-    #                                                                  "targetNicId": "eni-08bffbec11a1a794f",
-    #                                                                  "sessionNumber": "",
-    #                                                                  "filterRules": []
-    #                                                                  }
-    #                                             }
-    #                                         ]
-    #                           }
-    #     }
-    #     '''
-    #
-    #     request_parser = DriverRequestParser()
-    #     actions = request_parser.convert_driver_request_to_actions(request)
-    #     logger = Mock()
-    #     logging.disable(logging.DEBUG)
-    #
-    #     result = op.create(
-    #         ec2_client=self.ec2_client,
-    #         reservation=reservation,
-    #         actions=actions,
-    #         cancellation_context=Mock(),
-    #         logger=logger,
-    #         cloudshell=self.cloudshell)
-    #
-    #     self.assertTrue(result.Success, 'Was not able to create traffic mirroring')
-    #     self.fulfillments = result.Fulfillments
-    #
+    def test_create_traffic_mirroring(self):
+        tag_service = TagService(Mock())
+        session_number_service = SessionNumberService()
+        traffic_mirror_service = TrafficMirrorService()
+        cancellation_service = CommandCancellationService()
+        op = CreateTrafficMirrorOperation(tag_service,
+                                          session_number_service,
+                                          traffic_mirror_service,
+                                          cancellation_service)
+
+        reservation_context = Mock()
+        reservation_context.reservation_id = self.res.Id
+        reservation = ReservationModel(reservation_context)
+        reservation.blueprint = 'lalala'
+        reservation.owner = 'admin'
+        reservation.domain = 'global'
+
+        request = '''
+        {
+            "driverRequest": {
+                                "actions": [
+                                                {
+                                                    "actionId": "cbe6d3cb-daf3-4ec6-8a26-d2be9d500175",
+                                                    "actionTarget": null,
+                                                    "type": "CreateTrafficMirroring",
+                                                    "actionParams": {"type": "CreateTrafficMirroringParams",
+                                                                     "sourceNicId": "eni-0bf9b403bd8d36a79",
+                                                                     "targetNicId": "eni-060613fdccd935b67",
+                                                                     "sessionNumber": "",
+                                                                     "filterRules": []
+                                                                     }
+                                                }
+                                            ]
+                              }
+        }
+        '''
+
+        request_parser = DriverRequestParser()
+        actions = request_parser.convert_driver_request_to_actions(request)
+        logger = Mock()
+        logging.disable(logging.DEBUG)
+
+        cancellation_context = Mock()
+        cancellation_context.is_cancelled = False
+
+        result = op.create(
+            ec2_client=self.ec2_client,
+            reservation=reservation,
+            actions=actions,
+            cancellation_context=cancellation_context,
+            logger=logger,
+            cloudshell=self.cloudshell)
+
+        self.assertTrue(result.Success, 'Was not able to create traffic mirroring')
+        self.fulfillments = result.Fulfillments
+
     # def test_manual_tests(self):
     #     res = self.cloudshell.GetResourceDetails('AWS_APP i-02181554cd72946cf')
     #     print res
